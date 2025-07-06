07a79325c851f079dae21eb9d234ca1549b32cf4
Hanna Vorontsova <kievletterbox@gmail.com>
2025-07-06 17:13:33 +0300

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -7,18 +7,22 @@ function displayPoem(response){
       });
     }
 
-
-
 function generatePoem(event) {
   event.preventDefault();
-
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "03a1760f00b35ee83btdd2da764o002b";
   let context = "You are a romantic poem expert and love to write short poem and separate each line with <br />. Your mission is to generate a 4 line poem. Make sure to follow user instructions. Do not include a title to a poem. Sign the poem with 'SheCodes AI' inside a <strong></strong> element";
-  let promt = ` User instructions: Generate a Poem about ${instructionsInput.value}`;
+  let prompt = ` User instructions: Generate a Poem about ${instructionsInput.value}`;
   let apiURL =
     `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating a poem about ${instructionsInput.value}</div>`;
+
+
+
 
     axios.get(apiURL).then(displayPoem);
 
